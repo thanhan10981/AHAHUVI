67a4f6d885983f0abdc402f9eb4b2ab563f4b8eb
thanhan10981 <152773355+thanhan10981@users.noreply.github.com>
2024-09-07 14:23:59 +0700

    Add files via upload

---
+++ atm_momo.php
@@ -0,0 +1,64 @@
+<?php
+header('Content-type: text/html; charset=utf-8');
+
+function execPostRequest($url, $data)
+{
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($data))
+    );
+    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
+$endpoint = "https://test-payment.momo.vn/v2/gateway/api/create";
+$partnerCode = 'MOMOBKUN20180529';
+$accessKey = 'klm05TvNBzhg7h7j';
+$secretKey = 'at67qH6mk8w5Y1nAyMoYKMWACiEi2bsa';
+$orderInfo = "Thanh toán qua MoMo";
+$amount ="";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $amount=$_POST['amount'];
+}
+$orderId = time() . "";
+$redirectUrl = "http://127.0.0.1:5500/Home/thongtinhoadon.php";
+$ipnUrl = "http://127.0.0.1:5500/Home/thongtinhoadon.php";
+$extraData = "";
+    $partnerCode = $partnerCode;
+    $accessKey = $accessKey;
+    $secretkey = $secretKey;
+    $orderId = $orderId; 
+    $orderInfo = $orderInfo;
+    $amount = $amount;
+    $ipnUrl = $ipnUrl;
+    $redirectUrl = $redirectUrl;
+    $extraData =$extraData;
+
+    $requestId = time() . "";
+    $requestType = "payWithATM";
+    $rawHash = "accessKey=" . $accessKey . "&amount=" . $amount . "&extraData=" . $extraData . "&ipnUrl=" . $ipnUrl . "&orderId=" . $orderId . "&orderInfo=" . $orderInfo . "&partnerCode=" . $partnerCode . "&redirectUrl=" . $redirectUrl . "&requestId=" . $requestId . "&requestType=" . $requestType;
+    $signature = hash_hmac("sha256", $rawHash, $secretkey);
+    $data = array('partnerCode' => $partnerCode,
+        'partnerName' => "Test",
+        "storeId" => "MomoTestStore",
+        'requestId' => $requestId,
+        'amount' => $amount,
+        'orderId' => $orderId,
+        'orderInfo' => $orderInfo,
+        'redirectUrl' => $redirectUrl,
+        'ipnUrl' => $ipnUrl,
+        'lang' => 'vi',
+        'extraData' => $extraData,
+        'requestType' => $requestType,
+        'signature' => $signature);
+    $result = execPostRequest($endpoint, json_encode($data));
+    $jsonResult = json_decode($result, true);
+    header('Location: ' . $jsonResult['payUrl']);
+
+?>
