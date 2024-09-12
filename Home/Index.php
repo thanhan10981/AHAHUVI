a2f6c5cd41c438c732e71aa3b1431f3d92ce8aa1
thanhan10981 <152773355+thanhan10981@users.noreply.github.com>
2024-09-12 14:38:41 +0700

    Initial commit

---
+++ Home/Index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AHAHUVI.com - nhà sách trực tuyến</title>
+    <link rel="icon" href="../IMG/logo.png">
+</head>
+<body>
+<?php require("../Home/header.php") ?>
+<?php require("../Home/footer.php") ?>
+</body>
+</html>
---
+++ Home/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="../Css/header.css">
+</head>
+<body>
+        <div class="header">
+            <a href="../Home/Index.php"><img src="../IMG/header.jpg" alt="Ảnh quảng cáo"></a>
+        </div>
+    <div class="header-from">
+        <div class="nav_bar">
+            <a href="#" class="logo"><img src="../IMG/logo.png" alt="Logo"></a>
+            <a href="#" class="menu"><img src="../IMG/menu.png" alt="menu"></a>
+            <form action="#" class="search_form">
+                <input type="text" placeholder="Tìm kiếm...">
+                <button type="submit"><i class="fas fa-search"></i></button>
+            </form>
+            <div class="icon_group">
+            <a href="#">
+                <i class="fas fa-bell"></i>
+                <span>Thông Báo</span>
+            </a>
+            <a href="#">
+                <i class="fas fa-shopping-cart"></i>
+                <span>Giỏ Hàng</span>
+            </a>
+            <a href="#" class="user">
+                <i class="fas fa-user"></i>
+                <span>Tài Khoản</span>
+            </a>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
