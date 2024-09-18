d161748a6d0f459da920f5ebcdfe6958d22e60d0
truongngochan123 <144572122+truongngochan123@users.noreply.github.com>
2024-09-18 23:58:46 +0700

    Add files via upload

---
+++ Home/allproduct.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AHAHUVI.com - nhà sách trực tuyến</title>
+    <link rel="icon" href="../IMG/logo.png">
+    <link rel="stylesheet" href="../Css/allproduct.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+<body>
+<?php require("../Home/header.php") ?>
+<div class="than-tatcasp">
+    <div class="mucsp">
+        <ol class="danhmuc">
+            <li class="danhmuctheloai"><a href="">SÁCH TIẾNG VIỆT</a><i class="fa-solid fa-chevron-right"></i></li>
+            <li class="danhmuctheloai"><a href="">VĂN HỌC</a><i class="fa-solid fa-chevron-right"></i></li>
+            <li class="danhmuctheloai"><a href="">SÁCH TÔ MÀU DÀNH CHO TRẺ EM</a></li>
+        </ol>
+    </div>
+    <div class="tatcasp">
+    <div class="sidebar">
+        <h3>Nhóm Sản Phẩm</h3>
+        <ul class="filter-list">
+            <li><a href=" " >Tất Cả Nhóm Sản Phẩm</a></li>
+            <li><a href="" class="danhmucsp">Văn Học</a></li>
+            <li><a href="" class="theloaisp">Tiểu Thuyết</a></li>
+            <li><a href="" class="theloaisp">Truyện Ngắn - Tản Văn</a></li>
+            <li><a href="" class="theloaisp">Ngôn Tình</a></li>
+        </ul>
+        <button class="nutxemthem">Xem thêm</button>
+        <hr>
+        <h3>Giá</h3>
+        <ul class="filter-list">
+            <li><input type="radio" name="gia">0đ - 150.000đ</li>
+            <li><input type="radio" name="gia">150.000đ - 300.000đ</li>
+            <li><input type="radio" name="gia">300.000đ - 500.000đ</li>
+            <li><input type="radio" name="gia">500.000đ - 700.000đ</li>
+            <li><input type="radio" name="gia">700.000đ trở lên</li>
+        </ul>
+         <hr>
+        <h3>Nhà Cung Cấp</h3>
+        <ul class="filter-list">
+            <li><input type="radio" name="nhacungcap">Nhã Nam</li>
+            <li><input type="radio" name="nhacungcap">NXB Trẻ</li>
+            <li><input type="radio" name="nhacungcap">Đinh Tị</li>
+        </ul>
+    </div>
+    <div class="than-tatcasp-phai">
+        <div class="boloc">
+            <div class="iconboloc"><i class="fa-solid fa-filter"></i></div>
+            <div class="soluongsp">SL: <span>12</span></div>
+            <div class="giaboloc">Giá: <span>150.000đ - 300.000đ</span> <i class="fa-solid fa-xmark"></i></div>
+            <div class="nhacungcap">Nhà Cung Cấp: <span>Nhã Nam</span><i class="fa-solid fa-xmark"></i></div>
+            <button class="nutxoaboloc">Xóa tất cả</button>
+        </div>
+        <hr>
+        <div class="danhsachsp">
+            <div class="sp">
+                <img src="../IMG/sotayphatgiao.png" alt="Tôi Là Thầy Tướng Số 2">
+                <h4>Sổ tay biểu tượng phật giáo tây tạng</h4>
+                <span class="giamgia">50%</span>
+                <div class="giatien">
+                    <span class="giacu">120.000đ</span>
+                    <span class="giamoi">75.500đ</span>
+                </div>
+                <div class="sao">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+            </div>
+            <div class="sp">
+                <img src="../IMG/sotayphatgiao.png" alt="Tôi Là Thầy Tướng Số 2">
+                <h4>Sổ tay biểu tượng phật giáo tây tạng</h4>
+                <span class="giamgia">50%</span>
+                <div class="giatien">
+                    <span class="giacu">120.000đ</span>
+                    <span class="giamoi">75.500đ</span>
+                </div>
+                <div class="sao">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+            </div>
+            <div class="sp">
+                <img src="../IMG/sotayphatgiao.png" alt="Tôi Là Thầy Tướng Số 2">
+                <h4>Sổ tay biểu tượng phật giáo tây tạng</h4>
+                <span class="giamgia">50%</span>
+                <div class="giatien">
+                    <span class="giacu">120.000đ</span>
+                    <span class="giamoi">75.500đ</span>
+                </div>
+                <div class="sao">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+            </div>
+            <div class="sp">
+                <img src="../IMG/sotayphatgiao.png" alt="Tôi Là Thầy Tướng Số 2">
+                <h4>Sổ tay biểu tượng phật giáo tây tạng</h4>
+                <span class="giamgia">50%</span>
+                <div class="giatien">
+                    <span class="giacu">120.000đ</span>
+                    <span class="giamoi">75.500đ</span>
+                </div>
+                <div class="sao">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+            </div>
+            
+            
+        </div>
+        <div class="chuyentrang">
+            <a href="#"><i class="fa-solid fa-backward"></i></a>
+            <a href="#" class="sotrang active">1</a>
+            <a href="#" class="sotrang">2</a>
+            <a href="#" class="sotrang">3</a>
+            <a href="#" class="sotrang">5</a>
+            <a href="#"><i class="fa-solid fa-forward"></i></a>
+         </div>
+    </div>
+    </div>
+</div>
+<?php require("../Home/footer.php") ?>
+<script src=""></script>
+</body>
+</html>
